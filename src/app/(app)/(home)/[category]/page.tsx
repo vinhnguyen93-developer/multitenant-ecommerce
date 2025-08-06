ba0c2a3dc6bc20c38224dd888783c29fccbf3c19
VinhNguyen <vinh@cck.co.jp>
2025-08-06 22:10:54 +0700

    12: Filtering products

--- src/app/(app)/(home)/[category]/page.tsx
+++ src/app/(app)/(home)/[category]/page.tsx
@@ -3,6 +3,7 @@ import { dehydrate, HydrationBoundary } from "@tanstack/react-query"
 
 import { getQueryClient, trpc } from "@/trpc/server"
 
+import { ProductFilters } from "@/modules/products/ui/components/product-filters"
 import { ProductList, ProductListSkeleton } from "@/modules/products/ui/components/product-list"
 
 interface Props {
@@ -21,9 +22,19 @@ const Page = async ({ params }: Props) => {
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<ProductListSkeleton />}>
-        <ProductList category={category} />
-      </Suspense>
+      <div className="px-4 lg:px-12 py-8 flex flex-col gap-4">
+        <div className="grid grid-cols-1 lg:grid-cols-6 xl:grid-cols-8 gap-y-6 gap-x-12">
+          <div className="lg:col-span-2 xl:col-span-2">
+            <ProductFilters />
+          </div>
+
+          <div className="lg:col-span-4 xl:col-span-6">
+            <Suspense fallback={<ProductListSkeleton />}>
+              <ProductList category={category} />
+            </Suspense>
+          </div>
+        </div>
+      </div>
     </HydrationBoundary>
   )
 }
